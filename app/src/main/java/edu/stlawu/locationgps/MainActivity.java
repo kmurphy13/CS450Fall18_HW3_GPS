463e42e301f81732f08303622d20560eaf2beacc
Kira <kiramurphy13@yahoo.com>
2018-10-16 10:53:55 -0400

    Added information button

--- app/src/main/java/edu/stlawu/locationgps/MainActivity.java
+++ app/src/main/java/edu/stlawu/locationgps/MainActivity.java
@@ -3,31 +3,36 @@ package edu.stlawu.locationgps;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
+
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.GridView;
+
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity
         extends AppCompatActivity
-        implements Observer{
+        implements Observer {
 
     private TextView tv_lat;
     private TextView tv_lon;
@@ -35,6 +40,9 @@ public class MainActivity
     private TextView tv_startingLocation;
     private ScrollView scrollView;
     private Button controlButton;
+
+    private Timer t;
+    private Counter ctr;
 
     private Observable location;
     private LocationHandler handler = null;
@@ -45,8 +53,16 @@ public class MainActivity
 
     private Location startLocation = new Location("");
     private Location currentLocation = new Location("");
+    private Location previousLocation = new Location("");
+
+    private double velocityFromStart;
+    private double velocityFromPrev;
+    private double distanceFromStart;
+    private double distanceFromPrev;
+    private double recentTime = 0;
 
     private Boolean justStarted = true;
+
 
 
     @Override
@@ -68,32 +84,81 @@ public class MainActivity
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(
                     this,
-                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSION_REQUEST_CODE
             );
         }
 
-        final TextView tv_distances = findViewById(R.id.distances);
 
+        final TextView tv_distances = findViewById(R.id.distances);
+        final ScrollView sv_distances = findViewById(R.id.sv_distances);
 
         controlButton = findViewById(R.id.control_button);
         controlButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(controlButton.getText().equals("Start")){
-                    tv_startingLocation.setText("Starting Location: "+Double.toString(startLocation.getLatitude())+","+Double.toString(startLocation.getLongitude()));
+                if (controlButton.getText().equals("Start")) {
+                    tv_startingLocation.setText("Starting Location: " + Double.toString(startLocation.getLatitude()) + "," + Double.toString(startLocation.getLongitude()));
                     controlButton.setText("Checkpoint");
-                }
-                else if(controlButton.getText().equals("Checkpoint")){
-                    double distanceFromStart = startLocation.distanceTo(currentLocation);
-                    distanceFromStart = Math.round(distanceFromStart*100.0)/100.0;
-                    tv_distances.append("\t"+Double.toString(distanceFromStart)+"\n");
+                    previousLocation.setLatitude(startLocation.getLatitude());
+                    previousLocation.setLongitude(startLocation.getLongitude());
+                    t = new Timer();
+                    ctr = new Counter();
+                    t.scheduleAtFixedRate(ctr,0,1000);
 
 
+                } else if (controlButton.getText().equals("Checkpoint")) {
+
+                    distanceFromStart = startLocation.distanceTo(currentLocation);
+                    distanceFromPrev = previousLocation.distanceTo(currentLocation);
+
+                    DecimalFormat df = new DecimalFormat("00.00");
+
+
+                    tv_distances.append("\t\t\t" + df.format(distanceFromStart) + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"
+                            + df.format(distanceFromPrev) + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"+
+                            df.format(velocityFromStart) + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"+
+                            df.format(velocityFromPrev)+"\n"+
+                            "------------------------------------------------------------------------------------------------------------------------------------"+"\n");
+
+                    previousLocation.setLatitude(currentLocation.getLatitude());
+                    previousLocation.setLongitude(currentLocation.getLongitude());
+                    recentTime = ctr.count;
                 }
             }
 
+        });
+
+        ImageButton infoButton = findViewById(R.id.info_button);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_window, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+
+            }
         });
     }
 
@@ -103,7 +168,7 @@ public class MainActivity
 
     @Override
     public void onRequestPermissionsResult(
-        int requestCode, @NonNull String[] permissions,
+            int requestCode, @NonNull String[] permissions,
             @NonNull int[] grantResults) {
 
         super.onRequestPermissionsResult(
@@ -114,8 +179,7 @@ public class MainActivity
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 this.permissions_granted = true;
                 Log.i(LOGTAG, "Fine location permisssion granted.");
-            }
-            else {
+            } else {
                 this.permissions_granted = false;
                 Log.i(LOGTAG, "Fine location permisssion not granted.");
             }
@@ -126,34 +190,48 @@ public class MainActivity
     @Override
     public void update(Observable observable,
                        Object o) {
-        final ScrollView scrollview = ((ScrollView) findViewById(R.id.sv_distances));
         if (observable instanceof LocationHandler) {
             Location l = (Location) o;
-                final double lat = l.getLatitude();
-                final double lon = l.getLongitude();
+            final double lat = l.getLatitude();
+            final double lon = l.getLongitude();
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(justStarted){
-                            startLocation.setLatitude(lat);
-                            startLocation.setLongitude(lon);
-                            controlButton.setEnabled(true);
-                            justStarted = false;
-                        }else{
-                            currentLocation.setLatitude(lat);
-                            currentLocation.setLongitude(lon);
-                            tv_currentLocation.setText("Current Location: "+Double.toString(currentLocation.getLatitude())+","+Double.toString(currentLocation.getLongitude()));
-                        }
-                        scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (justStarted) {
+                        startLocation.setLatitude(lat);
+                        startLocation.setLongitude(lon);
+                        controlButton.setEnabled(true);
+                        justStarted = false;
+                    } else {
+                        currentLocation.setLatitude(lat);
+                        currentLocation.setLongitude(lon);
+                        tv_currentLocation.setText("Current Location: " + Double.toString(currentLocation.getLatitude()) + "," + Double.toString(currentLocation.getLongitude()));
                     }
-                });
+
+                }
+            });
 
 
         }
+
+
     }
 
+    class Counter extends TimerTask {
+        // Set the count to the count from the main activity
+        private int count = 0;
 
-
-
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Increase the count
+                    count++;
+                }
+            });
+        }
+    }
 }
